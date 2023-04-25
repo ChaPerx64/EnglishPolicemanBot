80e79b37799e38a0f013ae25fd615f67f2f0748e
@CheParx64 <chayan.par@gmail.com>
2023-04-25 19:05:50 +0200

    Deploy script made more universal

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+
 SRVR="root@188.225.86.226"
 EXECUTABLE="EnglishPolicemanBot.py"
 TARDIR="/root/bots/EnglishPolicemanBot"
-ssh $SRVR "mkdir $TARDIR"
-scp *.py ".env" "requirements.txt" "$SRVR:$TARDIR"
 ssh $SRVR << EOF
+pkill -f "$EXECUTABLE"
 cd "$TARDIR"
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+nohup python3 "$EXECUTABLE" >/dev/null 2>&1 &
 EOF
-bash run.sh
