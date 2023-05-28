0ee557ee5c051f523aebb1601b558434aa85cd84
@CheParx64 <chayan.par@gmail.com>
2023-05-28 13:25:07 +0200

    workflow updated yet again

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 
 pkill -f [E]nglishPolicemanBot.py
-git pull origin main
-git status
 python3 -m venv venv
 source venv/bin/activate
-nohup python3 "$EXECUTABLE" >/dev/null 2>&1 &
+nohup python3 "EnglishPolicemanBot.py" >/dev/null 2>&1 &
