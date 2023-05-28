99ea9a4747969f790fab7727b39221a0e4986552
@CheParx64 <chayan.par@gmail.com>
2023-05-28 12:43:49 +0200

    workflow updated

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 
-SRVR="root@188.225.86.226"
 EXECUTABLE="EnglishPolicemanBot.py"
 TARDIR="/root/bots/EnglishPolicemanBot"
-ssh $SRVR << EOF
+
 pkill -f "$EXECUTABLE"
 cd "$TARDIR"
+git pull origin main
+git status
 python3 -m venv venv
 source venv/bin/activate
 nohup python3 "$EXECUTABLE" >/dev/null 2>&1 &
-EOF
+
